446f0c557bb172c4c45c1f2738c5c851d3ee2ccc
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-17 22:23:36 +0200

    Add packer bootstrap code
    This tries to clone packer if it doesn't exist. Also add an autocommand
    to PackerCompile when the plugins file changes.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,12 @@
 -- vim: sw=2 sts=2 et
+
+-- packer bootstrap (requires git in path)
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
@@ -100,4 +108,16 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  -- auto sync if packer was bootstrapped
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
+  vim.cmd([[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+  ]])
 end)
