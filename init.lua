1c86e781cb2f30e1424351a77e8620f1b6377673
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-18 17:23:34 +0200

    Add some defaults

--- init.lua
+++ init.lua
@@ -6,6 +6,19 @@ vim.cmd([[
   filetype plugin indent on
 ]])
 
-vim.g.leader = ','
+vim.g.mapleader = ' '
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.hidden = true
+vim.opt.signcolumn = 'yes:2'
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.spell = true
+vim.opt.title = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.cmd("syntax enable")
 
 require('plugins')
