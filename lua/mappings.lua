ac9923436969f4dec13dcab1964af6959a085610
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-19 11:46:56 +0200

    More telescope mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,6 +19,13 @@ function mappings.define_mappings(mapper)
     mapper.map("n", "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<CR>", opts, "Movement", "switch_buffers", "Switch buffers")
     mapper.map("n", "<leader>/", "<cmd>Telescope live_grep<CR>", opts, "Editor", "live_grep", "Search a word")
     mapper.map("n", "<leader>:", "<cmd>Telescope command_history<CR>", opts, "Editor", "command_history", "Command history")
+    mapper.map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts, "Editor", "find_files", "Find files")
+    mapper.map("n", "<leader>gf", "<cmd>Telescope git_files<CR>", opts, "Editor", "git_files", "Git files")
+    mapper.map("n", "<leader>gs", "<cmd>Telescope git_status<CR>", opts, "Editor", "git_status", "Git status")
+    mapper.map("n", "<leader>gz", "<cmd>Telescope git_stash<CR>", opts, "Editor", "git_stash", "Git stash")
+    mapper.map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", opts, "Editor", "git_branches", "Git branches")
+    mapper.map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", opts, "Editor", "git_commits", "Git commits")
+    mapper.map("n", "<leader>gC", "<cmd>Telescope git_bcommits<CR>", opts, "Editor", "git_bcommits", "Git branch commits")
 end
 
 return mappings
