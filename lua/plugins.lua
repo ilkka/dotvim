7c5527cd7601382b8b0aa9a54985b93bf2384ae9
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-19 11:08:29 +0200

    Put mappings in their own file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,157 +2,144 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function()
-  -- Load all the plugins
-  use 'wbthomason/packer.nvim'
+    -- Load all the plugins
+    use 'wbthomason/packer.nvim'
 
-  use {
-    'dracula/vim',
-    config = function()
-      vim.cmd("colorscheme dracula")
-    end
-  }
-  use {
-    "lazytanuki/nvim-mapper",
-    config = function()
-      local M = require("nvim-mapper")
-      M.setup({
-        -- do not assign the default keymap (<leader>MM)
-        no_map = false,
-        -- where should ripgrep look for your keybinds definitions.
-        -- Default config search path is ~/.config/nvim/lua
-        search_path = vim.fn.stdpath('config').."\\lua",
-        -- search_path = "c:\\users\\ilkka.poutanen\\appdata\\local\\nvim\\lua",
-        -- what should be done with the selected keybind when pressing enter.
-        -- Available actions:
-        --   * "definition" - Go to keybind definition (default)
-        --   * "execute" - Execute the keybind command
-        action_on_enter = "execute",
-      })
-
-      -- mappings! This is _not_ the place for them tho
-      -- need to figure out this organisation
-      local opts = {silent = true, noremap = true}
-      M.map("n", "<leader>.", "<cmd>Telescope file_browser<CR>", opts, "Editor", "file_browser", "Browse files")
-      M.map("n", "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<CR>", opts, "Movement", "switch_buffers", "Switch buffers")
-      M.map("n", "<leader>/", "<cmd>Telescope live_grep<CR>", opts, "Editor", "live_grep", "Search a word")
-      M.map("n", "<leader>:", "<cmd>Telescope command_history<CR>", opts, "Editor", "command_history", "Command history")
-      M.map("n", "<leader>Q", "<cmd>bufdo bdelete<CR>", opts, "Editor", "close_all_buffers", "Close all buffers")
-      M.map("n", "<leader>k", "<cmd>nohlsearch<CR>", opts, "Editor", "no_search_highlight", "Turn off search highlights")
-      M.map("n", "<C-h>", "<C-w>h", opts, "Movement", "Switch window left")
-      M.map("n", "<C-j>", "<C-w>j", opts, "Movement", "Switch window down")
-      M.map("n", "<C-k>", "<C-w>k", opts, "Movement", "Switch window up")
-      M.map("n", "<C-l>", "<C-w>l", opts, "Movement", "Switch window right")
-      M.map("v", "<", "<gv", opts, "Editor", "Unindent selection")
-      M.map("v", ">", ">gv", opts, "Editor", "Indent selection")
-    end,
-    before = "telescope.nvim"
-  }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'sjl/splice.vim'
-  use {
-    'phaazon/hop.nvim',
-    config = function()
-      require('hop').setup()
-    end
-  }
-  use 'blackCauldron7/surround.nvim'
-  use {
-    'williamboman/nvim-lsp-installer',
-    config = function()
-      local lspinstall = require('nvim-lsp-installer')
-      local cmp = require('cmp_nvim_lsp')
-      lspinstall.on_server_ready(function(server)
-        local opts = {}
-        opts.capabilities = cmp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
-        server:setup(opts)
-      end)
-    end,
-    requires = {
-      'neovim/nvim-lspconfig'
-    },
-    after = {
-      'nvim-lspconfig',
-      'nvim-cmp'
+    use {
+        'dracula/vim',
+        config = function()
+            vim.cmd("colorscheme dracula")
+        end
     }
-  }
-  use 'vim-syntastic/syntastic'
-  use 'fsharp/vim-fsharp'
-  use {
-    'b3nj5m1n/kommentary',
-    config = function()
-      require('kommentary')
-      require('kommentary.config').use_extended_mappings()
-    end
-  }
-  use {
-    'hrsh7th/nvim-cmp',
-    requires = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/vim-vsnip'
-    },
-    config = function()
-      local cmp = require('cmp')
+    use {
+        "lazytanuki/nvim-mapper",
+        config = function()
+            local M = require("nvim-mapper")
+            M.setup({
+                -- do not assign the default keymap (<leader>MM)
+                no_map = false,
+                -- where should ripgrep look for your keybinds definitions.
+                -- Default config search path is ~/.config/nvim/lua
+                search_path = vim.fn.stdpath('config').."\\lua",
+                -- search_path = "c:\\users\\ilkka.poutanen\\appdata\\local\\nvim\\lua",
+                -- what should be done with the selected keybind when pressing enter.
+                -- Available actions:
+                --   * "definition" - Go to keybind definition (default)
+                --   * "execute" - Execute the keybind command
+                action_on_enter = "execute",
+            })
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end
+            -- do the mappings
+            require('mappings').define_mappings(M)
+        end,
+        before = "telescope.nvim"
+    }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'sjl/splice.vim'
+    use {
+        'phaazon/hop.nvim',
+        config = function()
+            require('hop').setup()
+        end
+    }
+    use 'blackCauldron7/surround.nvim'
+    use {
+        'williamboman/nvim-lsp-installer',
+        config = function()
+            local lspinstall = require('nvim-lsp-installer')
+            local cmp = require('cmp_nvim_lsp')
+            lspinstall.on_server_ready(function(server)
+                local opts = {}
+                opts.capabilities = cmp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+                server:setup(opts)
+            end)
+        end,
+        requires = {
+            'neovim/nvim-lspconfig'
         },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-
-      cmp.setup.cmdline('/', {
-        sources = {
-          { name = 'buffer' },
+        after = {
+            'nvim-lspconfig',
+            'nvim-cmp'
         }
-      })
+    }
+    use 'vim-syntastic/syntastic'
+    use 'fsharp/vim-fsharp'
+    use {
+        'b3nj5m1n/kommentary',
+        config = function()
+            require('kommentary')
+            require('kommentary.config').use_extended_mappings()
+        end
+    }
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip'
+        },
+        config = function()
+            local cmp = require('cmp')
 
-      cmp.setup.cmdline(':', {
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'vsnip' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            cmp.setup.cmdline('/', {
+                sources = {
+                    { name = 'buffer' },
+                }
+            })
+
+            cmp.setup.cmdline(':', {
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                })
+            })
+        end
+    }
+    use {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup()
+        end
+    }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+        config = function() require('telescope').load_extension('mapper') end
+    }
+
+    -- auto sync if packer was bootstrapped
+    if packer_bootstrap then
+        require('packer').sync()
     end
-  }
-  use {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup()
-    end
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
-    config = function() require('telescope').load_extension('mapper') end
-  }
 
-  -- auto sync if packer was bootstrapped
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-
-  vim.cmd([[
+    vim.cmd([[
     augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
     augroup end
-  ]])
+    ]])
 end)
 
--- vim: sw=2 sts=2 et nospell
+-- vim: sw=4 sts=4 et nospell
