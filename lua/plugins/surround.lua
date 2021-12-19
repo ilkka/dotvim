548720186d9329117759ffb4b90fd53d4704ed61
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-19 11:53:05 +0200

    Actually configure surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,1 +1,6 @@
-return 'blackCauldron7/surround.nvim'
+return {
+    "blackCauldron7/surround.nvim",
+    config = function()
+        require"surround".setup {mappings_style = "sandwich"}
+    end
+}
