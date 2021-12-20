86bb99302ba43ac78bbbdb2015be2f1a31b66faf
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-20 08:09:58 +0200

    Mappings for cmp

--- lua/plugins/plugin_nvim-cmp.lua
+++ lua/plugins/plugin_nvim-cmp.lua
@@ -17,6 +17,24 @@ return {
                     vim.fn["vsnip#anonymous"](args.body)
                 end
             },
+            mapping = {
+                ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+                ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+                ['<C-c>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+                ['<C-y>'] = cmp.config.disable,
+                ['<C-e>'] = cmp.mapping({
+                        i = cmp.mapping.abort(),
+                        c = cmp.mapping.close(),
+                    }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                --[[ ['<CR>'] = function (fallback)
+                    if cmp.visible() then
+                        cmp.mapping.confirm({ select = true })
+                    else
+                        fallback()
+                    end
+                end ]]
+            },
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'vsnip' },
