b40701ad45600abd31ac7dee2a0e71c21aeee499
Ilkka Poutanen <ilkka@ilkka.dev>
2022-04-17 21:10:40 +0300

    Actually config treesitter

--- lua/plugins/plugin_nvim-treesitter.lua
+++ lua/plugins/plugin_nvim-treesitter.lua
@@ -1,1 +1,28 @@
-return { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+return {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = {
+                -- treesitter is mega slow for elixir :(
+                -- "elixir",
+                "lua",
+                "json",
+                "javascript",
+                "typescript"
+            },
+            highlight = {
+                enable = true
+            },
+            incremental_selection = {
+                enable = true
+            },
+            textobjects = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            }
+        }
+    end
+}
